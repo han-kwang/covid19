e933a570b6efe65a55b4e895e3acc68fc7182e23
Han-Kwang Nienhuys <__@__>
2020-10-22 20:37:24 +0200

    Update nlcovidstats; features.
    - labels on relevant dates.
    - better handling with/without 7-day rolling average.
    - optimize placement of labels with the curves.
    - auto-download data from RIVM.

--- nlcovidstats.py
+++ nlcovidstats.py
@@ -11,7 +11,9 @@ import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+import urllib
+from pathlib import Path
+import time
 
 ## This is old stuff. It turns out that the 'casus' file is a pain to use.
 # # https://data.rivm.nl/covid-19/COVID-19_casus_landelijk.json
@@ -60,7 +62,25 @@ df_mun = df_mun.loc[~df_mun.Inwoners.isna()]
 import re
 df_mun.rename(index=lambda x: re.sub(r' \(gemeente\)$', '', x), inplace=True)
 
+df_restrictions = pd.DataFrame([
+    ('2020-05-11', 'Heropening, max 30'),
+    ('2020-06-01', 'Mondkapje OV'),
+    ('2020-07-01', "Max 100"),
+    ('2020-08-17', 'Scholen open Noord'),
+    ('2020-08-18', 'Max 6 gasten thuis'),
+    ('2020-08-31', 'Scholen open Midden'),
+    ('2020-09-20', 'Max 50/6, Horeca 01u'),
+    ('2020-09-29', 'Max 30/3, horeca 22u'),
+    ('2020-10-05', 'Mondkapjes VO'),
+    ('2020-10-14', 'Niveau Zeer Ernstig'),
+    ], columns=['Date', 'Description'])
+df_restrictions['Date'] = pd.to_datetime(df_restrictions['Date']) + pd.Timedelta('12:00:00')
+df_restrictions.set_index('Date', inplace=True)
+
+
 #%%
+
+
 
 def get_mun_data(df, mun, n_inw, lastday=-1):
     """Return dataframe for one municipality, added 'Delta' and 'Delta7r' columns.
@@ -85,11 +105,12 @@ def get_mun_data(df, mun, n_inw, lastday=-1):
     nc7 = nc.rolling(7, center=True).mean()
     nc7a = nc7.to_numpy()
     # last 3 elements are NaN, use mean of last 4 raw entries to
-    # get an estimated trend
+    # get an estimated trend and use exponential growth or decay
+    # for filling the data.
 
     nc1 = nc.iloc[-4:].mean() # mean number at t=-1.5 days
-    slope = (nc1 - nc7a[-4])/1.5
-    nc7.iloc[-3:] = nc7a[-4] + np.arange(1, 4)*slope
+    log_slope = (np.log(nc1) - np.log(nc7a[-4]))/1.5
+    nc7.iloc[-3:] = nc7a[-4] * np.exp(np.arange(1, 4)*log_slope)
 
     # 1st 3 elements are NaN
     nc7.iloc[:3] = np.linspace(0, nc7.iloc[3], 3, endpoint=False)
@@ -100,24 +121,29 @@ def get_mun_data(df, mun, n_inw, lastday=-1):
     return df1
 
 
-def get_t2_Rt(df1, delta_t, i0=-3):
+def get_t2_Rt(df1, delta_t, i0=-3, use_r7=True):
     """Return most recent doubling time and Rt."""
 
-    nc7 = df1['Delta7r']
+    # daily new cases
+    dnc = df1['Delta7r'] if use_r7 else df1['Delta']
     # exponential fit
     t_gen = 5.0 # generation time (d)
-    t_double = delta_t / np.log2(nc7.iloc[i0]/nc7.iloc[i0-delta_t])
+    t_double = delta_t / np.log2(dnc.iloc[i0]/dnc.iloc[i0-delta_t])
     Rt = 2**(t_gen / t_double)
     return t_double, Rt
 
-def add_labels(ax, labels, xpos, mindist=0.2):
+def add_labels(ax, labels, xpos, mindist_scale=1.0):
     """Add labels, try to have them avoid bumping.
 
 
     - labels: list of tuples (y, txt)
-    - mindist: minimum log10 distance.
+    - mindist_scale: set to >1 or <1 to tweak label spacing.
     """
     from scipy.optimize import fmin_cobyla
+
+    ymin, ymax = ax.get_ylim()
+    mindist = np.log10(ymax/ymin)*0.025*mindist_scale
+
 
     labels = sorted(labels)
 
@@ -146,13 +172,15 @@ def add_labels(ax, labels, xpos, mindist=0.2):
     for lgy, (_, txt) in zip(new_logys, labels):
         ax.text(xpos, 10**lgy,txt, verticalalignment='center')
 
-def plot_daily_trends(minpop=2e+5, ndays=100, lastday=-1, mun_regexp=None, mindist=0.04):
+def plot_daily_trends(minpop=2e+5, ndays=100, lastday=-1, mun_regexp=None,
+                      use_r7=True):
     """Plot daily-case trends.
 
     - minpop: minimum city population
     - lastday: up to this day.
     - mindist: minimum spacing of curve labels.
       Adjust depending on y range.
+    - use_r7: whether to use 7-day rolling average.
     """
 
     fig, ax = plt.subplots(figsize=(12, 6))
@@ -162,9 +190,8 @@ def plot_daily_trends(minpop=2e+5, ndays=100, lastday=-1, mun_regexp=None, mindi
     muns = df_mun.loc[df_mun['Inwoners'] > minpop]['Inwoners'].to_dict()
     muns['Nederland'] = float(df_mun.sum())
 
-    labels = [] # tuples (y, txt)
+    labels = [] # tuples (y, txt)f
     citystats = [] # tuples (Rt, T2, cp100k, cwk, popk, city_name)
-
     for mun, n_inw in muns.items():
 
         if mun_regexp and not re.match(mun_regexp, mun):
@@ -172,22 +199,18 @@ def plot_daily_trends(minpop=2e+5, ndays=100, lastday=-1, mun_regexp=None, mindi
 
         df1 = get_mun_data(df, mun, n_inw, lastday=lastday)
         df1 = df1.iloc[-ndays:]
-        use_r7 = True
-
         if mun == 'Nederland':
             print(df1.iloc[-3:][['Delta']]*n_inw)
 
 
-        fmt = 'o-' if ndays < 30 else '-'
+        fmt = 'o-' if ndays < 70 else '-'
+        psize = 5 if ndays < 30 else 3
 
-        if use_r7:
-            ax.semilogy(df1['Delta7r']*1e5, fmt, label=mun)
-        else:
-            ax.semilogy(df1['Delta']*1e5, fmt, label=mun)
-
+        dnc_column = 'Delta7r' if use_r7 else 'Delta'
+        ax.semilogy(df1[dnc_column]*1e5, fmt, label=mun, markersize=psize)
         delta_t = 7
-        i0 = -3
-        t_double, Rt = get_t2_Rt(df1, delta_t, i0=i0)
+        i0 = -3 if use_r7 else -1
+        t_double, Rt = get_t2_Rt(df1, delta_t, i0=i0, use_r7=use_r7)
         citystats.append((np.around(Rt, 2), np.around(t_double, 2),
                           np.around(df1['Delta'][-1]*1e5, 2),
                           int(df1['Delta7r'][-4] * n_inw * 7 + 0.5),
@@ -201,10 +224,16 @@ def plot_daily_trends(minpop=2e+5, ndays=100, lastday=-1, mun_regexp=None, mindi
             texp = f'×½: {-t_double:.2g} d'
 
         ax.semilogy(
-            df1.index[[i0-delta_t, i0]], df1['Delta7r'].iloc[[i0-delta_t, i0]]*1e5,
+            df1.index[[i0-delta_t, i0]], df1[dnc_column].iloc[[i0-delta_t, i0]]*1e5,
             'k--', zorder=-10)
 
-        labels.append((df1['Delta7r'][-1]*1e5, f' {mun} ({texp})'))
+        labels.append((df1[dnc_column][-1]*1e5, f' {mun} ({texp})'))
+
+
+    y_lab = ax.get_ylim()[0]
+    for res_t, res_d in df_restrictions['Description'].iteritems():
+        ax.text(res_t, y_lab, f'  {res_d}', rotation=90, horizontalalignment='center')
+
 
     dfc = pd.DataFrame.from_records(
         sorted(citystats), columns=['Rt', 'T2', 'C/100k', 'C/wk', 'Pop/k', 'Gemeente'])
@@ -213,10 +242,10 @@ def plot_daily_trends(minpop=2e+5, ndays=100, lastday=-1, mun_regexp=None, mindi
 
 
     lab_x = df1.index[-1] + pd.Timedelta('1.2 d')
-    add_labels(ax, labels, lab_x, mindist=mindist)
+    add_labels(ax, labels, lab_x)
 
 
-    ax.grid()
+    ax.grid(which='both')
 
     if use_r7:
         ax.axvline(df1.index[-4], color='gray')
@@ -226,17 +255,58 @@ def plot_daily_trends(minpop=2e+5, ndays=100, lastday=-1, mun_regexp=None, mindi
 
     #ax.set_ylim(0.05, None)
     ax.set_xlim(None, df1.index[-1] + pd.Timedelta('1 d'))
+    from matplotlib.ticker import LogFormatter, FormatStrFormatter
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%g'))
+    # Monkey-patch to prevent '%e' formatting.
+    LogFormatter._num_to_string = lambda _0, x, _1, _2: ('%g' % x)
+    ax.yaxis.set_minor_formatter(LogFormatter(minor_thresholds=(3, 1)))
     #plt.xticks(pd.to_dateTime(['2020-0{i}-01' for i in range(1, 9)]))
     ax.legend() # loc='lower left')
     for tl in ax.get_xticklabels():
         tl.set_ha('left')
 
+
+def update_csv(force=False):
+    """Update csv file (if not recently updated)."""
+
+    fpath = Path('data/COVID-19_aantallen_gemeente_cumulatief.csv')
+    if fpath.is_file():
+        local_file_data = fpath.read_bytes()
+    else:
+        local_file_data = None
+
+    if not force:
+        if fpath.is_file():
+            # estimated last update
+            tm = time.time()
+            loc_time = time.localtime(tm)
+            day_seconds = loc_time[3]*3600 + loc_time[4]*60 + loc_time[5]
+            tm_latest = tm - day_seconds + 14*3600 + 15*60
+            if tm_latest > tm:
+                tm_latest -= 86400
+
+            tm_file = fpath.stat().st_mtime
+            if tm_file > tm_latest:
+                print('Not updating file; seems to be recent enough.')
+                return
+
+
+    url = 'https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv'
+    print(f'Getting new file ...')
+    with urllib.request.urlopen(url) as response:
+        data_bytes = response.read()
+        if data_bytes == local_file_data:
+            print(f'{fpath}: already latest version.')
+        else:
+            fpath.write_bytes(html)
+            print(f'Wrote {fpath} .')
+
 if __name__ == '__main__':
 
 
-    # To refresh the data:
-    # wget -O data/COVID-19_aantallen_gemeente_cumulatief.csv https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv
+    update_csv()
 
+    plt.close('all')
     df = pd.read_csv('data/COVID-19_aantallen_gemeente_cumulatief.csv', sep=';')
     df = df.loc[~df.Municipality_code.isna()]
     df['Date_of_report'] = pd.to_datetime(df['Date_of_report'])
@@ -245,5 +315,5 @@ if __name__ == '__main__':
     plt.close('all')
 
     print(f'CSV most recent date: {df["Date_of_report"].iat[-1]}')
-    
-    plot_daily_trends(ndays=28, lastday=-1)
+
+    plot_daily_trends(ndays=40, lastday=-1, use_r7=True, minpop=2e5)
